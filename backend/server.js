bddcdbf8258bd5c60cb74764fe670b7d9ca6f9bf
Labh Bothra <labhbothra1380@gmail.com>
2025-08-24 22:01:41 +0530

    Done with Domain

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.use(cors({
       'http://127.0.0.1:5174',
       'https://ecommerce-fashion-app.vercel.app',
       'https://ecommerce-fashion-app-dashboard.vercel.app',
-      'https://flauntbynishi.com'
+      'https://www.flauntbynishi.com'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','HEAD'],
